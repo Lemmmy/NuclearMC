62ab402554a53975020d8497f9b567212e1fc770
Drew Lemmy <lemonplays@gmail.com>
2015-04-12 15:29:15 +0100

    gui start

--- src/main/java/net/teamdentro/nuclearmc/NuclearMC.java
+++ src/main/java/net/teamdentro/nuclearmc/NuclearMC.java
@@ -1,5 +1,7 @@
 package net.teamdentro.nuclearmc;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -7,6 +9,7 @@ import java.util.logging.Logger;
 
 public class NuclearMC {
 	private static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	private static boolean gui;
 	
 	public static Logger getLogger() {
 		return LOGGER;
@@ -15,6 +18,9 @@ public class NuclearMC {
 	public static long MAIN_THREAD_ID;
 	
 	public static void main(String[] args) {
+		List<String> arguments = Arrays.asList(args);
+		gui = arguments.contains("-g") || arguments.contains("--gui");
+
 		MAIN_THREAD_ID = Thread.currentThread().getId();
 		
 		getLogger().setLevel(Level.ALL);
@@ -23,13 +29,21 @@ public class NuclearMC {
 		for (Handler handler : getLogger().getHandlers()) {
 			getLogger().removeHandler(handler);
 		}
-		
-		ConsoleHandler chandler = new ConsoleHandler();
-		chandler.setFormatter(new ConsoleFormatter());
-		chandler.setLevel(Level.ALL);
-		getLogger().addHandler(chandler);
-		
+
+		if (gui) {
+
+		} else {
+			ConsoleHandler chandler = new ConsoleHandler();
+			chandler.setFormatter(new ConsoleFormatter());
+			chandler.setLevel(Level.ALL);
+			getLogger().addHandler(chandler);
+		}
+
 		Server server = new Server();
 		server.run();
+	}
+
+	public static boolean isGUI() {
+		return gui;
 	}
 }
