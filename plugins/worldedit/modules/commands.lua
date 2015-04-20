0bf54be964292b7664586cb7f09e2c330cb0f56a
Lemmmy <lemonplays@gmail.com>
2015-04-20 21:13:59 +0100

    babby pooshed

--- plugins/worldedit/modules/commands.lua
+++ plugins/worldedit/modules/commands.lua
@@ -5,17 +5,21 @@ Commands.addCommand("set", {
 	
 	execute = function(sender, args)
 		if #args == 1 then
+			local block = Blocks.getBlock(args[1])
 			WorldEditSelection(2, sender, function(user, points) 
-				local block = Blocks.getBlock(args[1])
 				local changecount = 0
 
-				for y=points[1].y,points[2].y do
-					for x=points[1].x,points[2].x do
-						for z=points[1].z,points[2].z do
+				for y=points[1].y, points[2].y, points[2].y<points[1].y and -1 or 1 do
+					for x=points[1].x, points[2].x, points[2].x<points[1].x and -1 or 1 do
+						for z=points[1].z, points[2].z, points[2].z<points[1].z and -1 or 1 do
 							changecount = changecount + 1
 							user:getLevel():setBlockNotify(x, y, z, Blocks.getJBlock(block))
 						end
 					end
+				end
+
+				for k,v in ipairs(points) do
+					user:getLevel():setBlockNotify(v.x, v.y, v.z, Blocks.getJBlock(block))
 				end
 
 				user:sendMessage(Utils.substitute(DefaultSettings.BlockChangeMessage,
